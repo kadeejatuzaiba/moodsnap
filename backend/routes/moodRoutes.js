327bd9377ea236a8efab5e685a4bd75767e29f0c
kadeejatu Zaiba <kadeejazaiba786@gmail.com>
2026-04-27 19:37:01 +0530

    fix case sensitive issue

--- backend/routes/moodRoutes.js
+++ backend/routes/moodRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Mood = require("../models/mood");
+const Mood = require("../models/Mood");
 
 // create mood
 router.post("/", async (req, res) => {
