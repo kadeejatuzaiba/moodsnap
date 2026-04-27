4c3a104f991b08146f0c9df1fbc68f432426cdd2
kadeejatu Zaiba <kadeejazaiba786@gmail.com>
2026-04-27 13:44:28 +0530

    moodsnap mongodb fixed

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,10 @@ app.use(cors());
 app.use(express.json());
 
 // connect DB
-mongoose.connect("mongodb://localhost:27017/moodsnap")
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("DB Connected"))
     .catch(err => console.log("MongoDB connection error:", err));
 
-// test route
-app.get("/", (req, res) => {
-    res.send("API running");
-});
 const moodRoutes = require("./routes/moodRoutes");
 app.use("/api/moods", moodRoutes);
 const Mood = require("./models/mood");
@@ -33,12 +29,12 @@ app.get("/api/stats", async (req, res) => {
             { $match: matchStage },
             { $group: { _id: "$mood", count: { $sum: 1 } } }
         ]);
-        
+
         const formattedStats = stats.reduce((acc, curr) => {
             acc[curr._id] = curr.count;
             return acc;
         }, {});
-        
+
         res.json(formattedStats);
     } catch (err) {
         res.status(500).json({ error: "Server error" });
